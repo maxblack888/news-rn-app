26ba5334b744c8863e7b6185a6b307c71521d0c7
maxblack888 <kshitijgautam8800@outlook.com>
2019-07-22 16:32:51 +0530

    first commit

---
+++ app/modules/home/constants.js
@@ -0,0 +1,8 @@
+export const NAME = 'home';
+
+export const RETRIEVING_HEADLINES = 'home/RETRIEVING_HEADLINES';
+export const HEADLINES_AVAILABLE = 'home/HEADLINES_AVAILABLE';
+export const HEADLINES_ERROR = 'home/HEADLINES_ERROR';
+
+export const API_URL = 'https://newsapi.org/v2/top-headlines?';
+export const API_KEY = '3a0941a7ac064cb9b0196fe0badd0129';
